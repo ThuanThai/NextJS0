a6ed6f3691ab4418717d3a32b2bf3742c26e2ece
ThaiThuan <s3877024@rmit.edu.vn>
2024-05-09 00:38:57 +0700

    update routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import UsersPage from './screens/users.page';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <div>Hello world!</div>,
+    },
+    {
+        path: '/users',
+        element: <UsersPage />,
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
---
+++ src/screens/users.page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import UsersTable from '../components/users/users.table';
+
+const UsersPage = () => {
+    return (
+        <div>
+            <h1>Manage Users</h1>
+            <UsersTable></UsersTable>
+        </div>
+    );
+};
+
+export default UsersPage;
